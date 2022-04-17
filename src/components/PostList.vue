2f0043add4ee2e1aeefea1e76ea8163ed487d663
Daniel Eicher <d.eicher@mailbox.org>
2022-04-17 14:45:06 +0200

    Using lifecycle hooks in setup

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,15 +7,17 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from 'vue';
 import SinglePost from './SinglePost.vue';
 
 export default {
   props: ['posts'],
   components: { SinglePost },
   setup(props) {
-    console.log(props.posts);
+    onMounted(() => console.log('component mounted'));
+    onUnmounted(() => console.log('component unmounted'));
+    onUpdated(() => console.log('component updated'));
   },
-  components: { SinglePost },
 };
 </script>
 
