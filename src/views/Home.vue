678209530022f8c19e0c8498dab40ee08a54988f
Daniel Eicher <d.eicher@mailbox.org>
2022-04-17 11:44:09 +0200

    Add reactive refs to composition api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    home
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">add 1 to age</button>
+    <input type="text" v-model="name" />
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'Home',
   setup() {
-    console.log('setup');
+    let name = ref('Daniel');
+    let age = ref(36);
+
+    const handleClick = () => {
+      name.value = 'Luigi';
+      age.value = 23;
+    };
+
+    return { name, age, handleClick };
   },
 };
 </script>
