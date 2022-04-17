83b720fee53aa1450495b22778eea6722b6b77dd
Daniel Eicher <d.eicher@mailbox.org>
2022-04-17 14:24:28 +0200

    Using props in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,48 +1,28 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search" />
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @click="handleClick">stop watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue';
+import { ref } from 'vue';
+import PostList from '../components/PostList.vue';
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
-    const search = ref('');
-    const names = ref([
-      'mario',
-      'yoshi',
-      'luigi',
-      'toad',
-      'bowser',
-      'koopa',
-      'peach',
+    const posts = ref([
+      {
+        title: 'welcome to the blog',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore cum voluptates a, commodi molestiae esse delectus, explicabo ea nam quasi quia sunt, omnis ipsam accusamus iusto minus accusantium reiciendis provident. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam soluta reiciendis maiores aut atque aliquid repellendus, pariatur, quam a labore ab explicabo laudantium vero debitis unde blanditiis assumenda dolorem. Ratione. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod voluptate aspernatur officiis blanditiis qui velit maiores omnis fuga est natus molestiae, repellat beatae maxime ab voluptatibus? Delectus repellendus aliquid quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. A iusto blanditiis maxime, expedita natus praesentium, repellat adipisci, aliquid est maiores vero eius laudantium vel excepturi alias voluptatum nemo id suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta corporis cumque facilis, eum, suscipit nam blanditiis iure a necessitatibus modi fugit ea dicta deleniti, tenetur dignissimos molestias dolorum delectus quod!',
+        id: 1,
+      },
+      { title: 'top 5 css tips', body: 'Lorem ipsum', id: 2 },
     ]);
 
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran');
-    });
-
-    const stopEffect = watchEffect(() => {
-      console.log('watchEffect function ran', search.value);
-    });
-
-    const matchingNames = computed(() => {
-      return names.value.filter(name => name.includes(search.value));
-    });
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    };
-
-    return { names, matchingNames, search, handleClick };
+    return { posts };
   },
 };
 </script>
