bd434ee46b95c342024617e37fb7dae62c75c197
Daniel Eicher <d.eicher@mailbox.org>
2022-04-18 07:11:57 +0200

    Outsource data in db and add async await

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -15,17 +17,25 @@ export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: 'welcome to the blog',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore cum voluptates a, commodi molestiae esse delectus, explicabo ea nam quasi quia sunt, omnis ipsam accusamus iusto minus accusantium reiciendis provident. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam soluta reiciendis maiores aut atque aliquid repellendus, pariatur, quam a labore ab explicabo laudantium vero debitis unde blanditiis assumenda dolorem. Ratione. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod voluptate aspernatur officiis blanditiis qui velit maiores omnis fuga est natus molestiae, repellat beatae maxime ab voluptatibus? Delectus repellendus aliquid quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. A iusto blanditiis maxime, expedita natus praesentium, repellat adipisci, aliquid est maiores vero eius laudantium vel excepturi alias voluptatum nemo id suscipit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta corporis cumque facilis, eum, suscipit nam blanditiis iure a necessitatibus modi fugit ea dicta deleniti, tenetur dignissimos molestias dolorum delectus quod!',
-        id: 1,
-      },
-      { title: 'top 5 css tips', body: 'Lorem ipsum', id: 2 },
-    ]);
-    const showPosts = ref(true);
+    const posts = ref([]);
+    const error = ref(null);
 
-    return { posts, showPosts };
+    const load = async () => {
+      try {
+        let data = await fetch(' http://localhost:3000/posts');
+        if (!data.ok) {
+          throw Error('no data available');
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.error(error.value);
+      }
+    };
+
+    load();
+
+    return { posts, error };
   },
 };
 </script>
